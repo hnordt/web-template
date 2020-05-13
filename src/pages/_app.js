97c8f285b4e29c76e546628a4375482c7c69c234
Heliton Nordt <hnordt@hnordt.com>
2020-05-12 23:11:32 -0300

    refactor: Cleanup

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,18 +1,6 @@
 import React from "react"
-import Head from "next/head"
 import "styles/index.css"
 
 export default function App(props) {
-  return (
-    <>
-      <Head>
-        <meta name="description" content="This is a web app" />
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900&display=swap"
-        />
-      </Head>
-      <props.Component {...props.pageProps} />
-    </>
-  )
+  return <props.Component {...props.pageProps} />
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,12 +2,16 @@ import React from "react"
 import BaseDocument, { Html, Head, Main, NextScript } from "next/document"
 
 export default class Document extends BaseDocument {
-  static getInitialProps = BaseDocument.getInitialProps
-
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <meta name="description" content="This is a web app" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900&display=swap"
+          />
+        </Head>
         <body className="bg-gray-100 antialiased">
           <Main />
           <NextScript />
