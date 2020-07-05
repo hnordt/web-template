2d6c2fd5fa5a8b7ceb4acc2ad11cb4098994a76b
Heliton Nordt <hnordt@hnordt.com>
2020-07-05 12:31:09 -0300

    chore: Improvements

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,12 +7,13 @@ export default class Document extends BaseDocument {
       <Html lang="en">
         <Head>
           <meta name="description" content="" />
+          <link rel="icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Inter:100,200,300,400,500,600,700,800,900&display=swap"
           />
         </Head>
-        <body className="bg-gray-100 antialiased">
+        <body className="bg-cool-gray-100 antialiased">
           <Main />
           <NextScript />
         </body>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from "react"
-import Screen from "components/Screen"
 import Layout from "components/Layout"
 
 export default function Home() {
   return (
-    <Screen title="Home">
-      <Layout>
-        <h1>It works!</h1>
-      </Layout>
-    </Screen>
+    <Layout title="Home">
+      <h1>It works!</h1>
+    </Layout>
   )
 }
