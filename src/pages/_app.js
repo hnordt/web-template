d728c631f639f43164ae2558e44bdcff6fd4c827
Heliton Nordt <hnordt@hnordt.com>
2020-08-31 11:36:02 -0300

    feat: React Toastify

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,20 @@
 import "focus-visible"
+import "react-toastify/dist/ReactToastify.css"
 import "styles/index.css"
 
 import React from "react"
+import { ToastContainer, Flip, toast } from "react-toastify"
 
 export default function App(props) {
-  return <props.Component {...props.pageProps} />
+  return (
+    <>
+      <props.Component {...props.pageProps} />
+      <ToastContainer
+        toastClassName="pl-5"
+        closeButton={null}
+        position={toast.POSITION.TOP_RIGHT}
+        transition={Flip}
+      />
+    </>
+  )
 }
