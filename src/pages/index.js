5be4b2df7b0e839e59c0f3bdfed2cdf18bc2b22c
Heliton Nordt <hnordt@hnordt.com>
2020-10-09 22:33:24 -0300

    chore: Minor change

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "components/Layout"
 
-export default function Home() {
+export default function Index() {
   return (
     <Layout title="Home">
       <h1>It works!</h1>
