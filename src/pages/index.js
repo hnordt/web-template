52e593b838c01d3e0de214fc4e120aa1b0ecd7d6
Heliton Nordt <hnordt@hnordt.com>
2020-10-09 22:32:55 -0300

    chore: Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,46 +1,10 @@
 import React from "react"
-import { Switch, Route, Link } from "react-router-dom"
 import Layout from "components/Layout"
 
-function Home() {
-  return <h2>Home</h2>
-}
-
-function About() {
-  return <h2>About</h2>
-}
-
-function Users() {
-  return <h2>Users</h2>
-}
-
-export default function Index() {
+export default function Home() {
   return (
     <Layout title="Home">
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/users">
-          <Users />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+      <h1>It works!</h1>
     </Layout>
   )
 }
