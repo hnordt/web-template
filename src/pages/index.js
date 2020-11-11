03718bcda5b9cf2d7cd6d8dc5dfe609ba440a2ae
Heliton Nordt <hnordt@hnordt.com>
2020-11-11 17:54:06 -0300

    chore: Revert index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,5 @@
-import Breadcrumb from "components/Breadcrumb"
-import Breadcrumbs from "components/Breadcrumbs"
 import React from "react"
-import { BsArrowDown } from "react-icons/bs"
-import Tag from "../components/Tag"
-import Tags from "../components/Tags"
 
 export default function Index() {
-  return (
-    <div>
-      <Breadcrumbs>
-        <Breadcrumb href="/requests">Requests</Breadcrumb>
-        <Breadcrumb href="/threats">Threats</Breadcrumb>
-        <Breadcrumb current>By user</Breadcrumb>
-      </Breadcrumbs>
-    </div>
-  )
+  return <main className="p-6">It works!</main>
 }
