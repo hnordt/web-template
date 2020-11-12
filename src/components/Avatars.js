3df297516633d4fb578a3aa16f33c3fb0c41058b
Dickson <dicksonmelo@gmail.com>
2020-11-12 17:11:10 -0300

    chore: Add switch component

--- src/components/Avatars.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export function Avatars(props) {
-  return <picture className="flex">{props.children}</picture>
-}
---
+++ src/components/Toggle.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+export default function Toggle(props) {
+  return (
+    <div>
+      <span
+        className="bg-gray-200 relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:shadow-outline"
+        onClick={() => props.onChange?.(!props.on)}
+        role="checkbox"
+        tabIndex={0}
+        aria-checked="false"
+      >
+        <span
+          aria-hidden="true"
+          className="translate-x-0 inline-block h-5 w-5 rounded-full bg-white shadow transform transition ease-in-out duration-200"
+        />
+      </span>
+    </div>
+  )
+}
