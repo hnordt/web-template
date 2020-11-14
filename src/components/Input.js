80d9c859758dfcb6090044865bee9de282548ea6
Heliton Nordt <hnordt@hnordt.com>
2020-11-14 19:29:02 -0300

    feat: Add Input and TextArea

---
+++ src/components/Input.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function Input(props) {
+  return (
+    <label>
+      {props.label && <span className="text-gray-700">{props.label}</span>}
+      <input
+        className="form-input block mt-1 w-full"
+        type={props.type}
+        name={props.name}
+        value={props.value}
+        placeholder={props.placeholder}
+        onChange={(e) => props.onChange?.(e.target.value)}
+      />
+    </label>
+  )
+}
---
+++ src/components/TextArea.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+export default function TextArea(props) {
+  return (
+    <label>
+      {props.label && <span className="text-gray-700">{props.label}</span>}
+      <textarea
+        className="form-textarea block mt-1 w-full"
+        name={props.name}
+        rows={props.rows ?? 3}
+        value={props.value}
+        placeholder={props.placeholder}
+        onChange={(e) => props.onChange?.(e.target.value)}
+      />
+    </label>
+  )
+}
