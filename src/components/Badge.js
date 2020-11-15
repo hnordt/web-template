d75e6e0080e73f1834a6b1aafa938718176e675a
Heliton Nordt <hnordt@hnordt.com>
2020-11-15 19:46:39 -0300

    fix: Replace Tags / Tag by Badge

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -1,11 +1,11 @@
 import React from "react"
 import cn from "classnames"
 
-export default function Tag(props) {
+export default function Badge(props) {
   return (
     <span
       className={cn(
-        "inline-flex items-center px-2 py-0.5 text-xs font-medium rounded",
+        "inline-flex items-center px-3 py-0.5 text-sm font-medium rounded-full",
         props.variant === "primary" && "text-blue-800 bg-blue-100",
         props.variant === "secondary" && "text-gray-800 bg-gray-100",
         props.variant === "success" && "text-green-800 bg-green-100",
--- src/components/Tags.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-export default function Tags(props) {
-  return <div className="flex items-center space-x-4">{props.children}</div>
-}
