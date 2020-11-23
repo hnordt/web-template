24611b324fa4037f9a2071aa8fa18b4b070920be
Dickson <dicksonmelo@gmail.com>
2020-11-23 15:18:28 -0300

    chore: Add photo layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
 import React from "react"
+import { MdClose } from "react-icons/md"
 
-export default function Index() {
-  return <main className="p-6">It works!</main>
+export default function Index(props) {
+  return (
+    <div className="w-1/4 h-1/2 border">
+      <div className="flex flex justify-between">
+        <h1 className="text-gray-500 text-lg font-semibold">Contact Details</h1>
+        <span className="relative left-0">
+          <MdClose color={"gray"} size={24} />
+        </span>
+      </div>
+      <hr />
+      <div>{props.photo}</div>
+    </div>
+  )
 }
