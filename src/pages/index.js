83e1f3ba8752b73bfdc2d3fd30e87a1727ec2859
Dickson <dicksonmelo@gmail.com>
2020-11-24 11:23:35 -0300

    chore: Implement chart layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,66 +1,71 @@
 import React from "react"
-import {
-  LineChart,
-  Line,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from "recharts"
+import { RiKnifeBloodFill } from "react-icons/ri"
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts"
 
 const data = [
   {
+    date: "SEP 10",
     name: "Page A",
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
+    Requests: 4500,
+    OtherThing: 3400,
+    OneMoreThing: 2400,
   },
   {
+    date: "SEP 11",
     name: "Page B",
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
+    Requests: 3000,
+    OtherThing: 1398,
+    OneMoreThing: 3210,
   },
   {
+    date: "SEP 12",
     name: "Page C",
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
+    Requests: 2000,
+    OtherThing: 9800,
+    OneMoreThing: 7290,
   },
   {
+    date: "SEP 13",
     name: "Page D",
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
+    Requests: 2780,
+    OtherThing: 3908,
+    OneMoreThing: 3000,
   },
   {
+    date: "SEP 14",
     name: "Page E",
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
+    Requests: 1890,
+    OtherThing: 4800,
+    OneMoreThing: 2181,
   },
   {
+    date: "SEP 15",
     name: "Page F",
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
+    Requests: 6390,
+    OtherThing: 4000,
+    OneMoreThing: 2500,
   },
   {
+    date: "SEP 16",
     name: "Page G",
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
+    Requests: 3490,
+    OtherThing: 4300,
+    OneMoreThing: 2100,
+    eixoY: [0, 10000],
   },
 ]
 
-export default function LineChartJS() {
+const toolTipStyle = {
+  font: "bold",
+}
+
+function LineChartJS(props) {
   return (
     <div className="w-full max-w-md">
       <LineChart
-        width={900}
-        height={300}
-        data={data}
+        width={1500}
+        height={500}
+        data={props.data}
         margin={{
           top: 5,
           right: 30,
@@ -69,20 +74,66 @@ export default function LineChartJS() {
         }}
       >
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="name" />
-        <YAxis />
-        <Tooltip />
-        <Legend />
+        <XAxis
+          dataKey="date"
+          tickLine={false}
+          axisLine={{
+            stroke: "#D3D3D3",
+            strokeWidth: 2,
+          }}
+          tickMargin={8}
+        />
+        <YAxis
+          dataKey="eixoY"
+          tickLine={{
+            stroke: "black",
+            strokeWidth: 1.5,
+          }}
+          axisLine={{
+            stroke: "#D3D3D3",
+            strokeWidth: 2,
+          }}
+          type="number"
+          scale="linear"
+          tickMargin={10}
+        />
+        <Tooltip itemStyle={toolTipStyle} />
         <Line
           type="monotone"
-          dataKey="pv"
-          stroke="#8884d8"
+          dataKey="OtherThing"
+          stroke="#18F"
+          strokeWidth={2}
           activeDot={{
-            r: 8,
+            r: 6,
           }}
         />
-        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+        <Line
+          type="monotone"
+          dataKey="Requests"
+          stroke="orange"
+          strokeWidth={2}
+          activeDot={{
+            r: 6,
+          }}
+        />
+        <Line
+          type="monotone"
+          dataKey="OneMoreThing"
+          stroke="red"
+          strokeWidth={2}
+          activeDot={{
+            r: 6,
+          }}
+        />
       </LineChart>
+    </div>
+  )
+}
+
+export default function Index() {
+  return (
+    <div>
+      <LineChartJS data={data} />
     </div>
   )
 }
