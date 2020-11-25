515c6fbda7b3ec000b3fa7d7f6691008335cc7d5
Dickson <dicksonmelo@gmail.com>
2020-11-25 14:45:09 -0300

    chore: Add threat card layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function NewReportLayout() {
+function Categories() {
   return (
     <div className="flex w-1/2 border rounded-xl shadow-xl">
       <div className="m-5 mt-6 w-20 h-20 bg-blue-100 rounded-full">
@@ -24,5 +24,43 @@ export default function NewReportLayout() {
         </span>
       </div>
     </div>
+  )
+}
+
+function SecurityThreats() {
+  return (
+    <div className="flex w-1/2 border rounded-xl shadow-xl">
+      <div className="m-5 mt-5 w-20 h-20 bg-red-100 rounded-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          version="1.1"
+          width="30"
+          height="30"
+          viewBox="-0.5 -0.5 24 24"
+          className="mt-6 mx-auto text-green-600 fill-current"
+        >
+          <path
+            fill="red"
+            d="M12,2A9,9 0 0,0 3,11C3,14.03 4.53,16.82 7,18.47V22H9V19H11V22H13V19H15V22H17V18.46C19.47,16.81 21,14 21,11A9,9 0 0,0 12,2M8,11A2,2 0 0,1 10,13A2,2 0 0,1 8,15A2,2 0 0,1 6,13A2,2 0 0,1 8,11M16,11A2,2 0 0,1 18,13A2,2 0 0,1 16,15A2,2 0 0,1 14,13A2,2 0 0,1 16,11M12,14L13.5,17H10.5L12,14Z"
+          />
+        </svg>
+      </div>
+      <div className="py-8">
+        <span className="block mb-1 text-xl">Security Threats</span>
+        <span className="text-gray-400 text-lg">
+          No security threats found for bittorrent.com
+        </span>
+      </div>
+    </div>
+  )
+}
+
+export default function Index() {
+  return (
+    <>
+      <Categories />
+      <SecurityThreats />
+    </>
   )
 }
