0f3df17b852caa5e73f82032f87049d7dcfe26a9
Heliton Nordt <hnordt@hnordt.com>
2020-12-23 00:24:19 -0300

    chore: Fixes

--- next.config.js
+++ next.config.js
@@ -1,46 +1,4 @@
-const fs = require("fs")
-const dotenv = require("dotenv")
-
-let oldAppEnv = {}
-let oldAppLocalEnv = {}
-
-try {
-  oldAppEnv = dotenv.parse(Buffer.from(fs.readFileSync("../.env")))
-} catch {}
-
-try {
-  oldAppLocalEnv = dotenv.parse(Buffer.from(fs.readFileSync("../.env.local")))
-} catch {}
-
-const assetPrefix =
-  process.env.NODE_ENV === "development" ? "" : "/new-reporting"
-
 module.exports = {
-  env: {
-    ...[
-      "API_URL",
-      "AUTH0_CLIENT_ID",
-      "AUTH0_SUBDOMAIN",
-      "GTM_KEY",
-      "SEGMENT_ID",
-      "WHITELABEL_SEGMENT",
-      "LOCATION_LOOKUP_URL",
-      "IPSTACK_KEY",
-      "ROLLBAR_TOKEN",
-      "COMMIT_REF",
-    ].reduce(
-      (acc, v) => ({
-        ...acc,
-        [`NEXT_PUBLIC_${v}`]:
-          process.env[`REACT_APP_${v}`] ||
-          oldAppLocalEnv[`REACT_APP_${v}`] ||
-          oldAppEnv[`REACT_APP_${v}`],
-      }),
-      {}
-    ),
-    NEXT_PUBLIC_ASSET_PREFIX: assetPrefix,
-  },
-  assetPrefix,
   async rewrites() {
     return [
       {
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,25 @@ import { ReactQueryDevtools } from "react-query/devtools"
 import ReportingAllOrganizationsScreen from "screens/ReportingAllOrganizationsScreen"
 
 export default function Reporting() {
+  const [ready, setReady] = React.useState(false)
+
+  React.useEffect(() => {
+    function handleMessage(e) {
+      if (e.data.token) {
+        localStorage.setItem("token", e.data.token)
+        setReady(true)
+      }
+    }
+
+    window.addEventListener("message", handleMessage, false)
+
+    return () => window.removeEventListener("message", handleMessage, false)
+  }, [])
+
+  if (!ready) {
+    return null
+  }
+
   return (
     <>
       <ReportingAllOrganizationsScreen />
