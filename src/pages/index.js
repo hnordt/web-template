42e58380c139f5b333f3278f304fb919e7b27fb2
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-03-12 12:08:02 -0300

    chore: Cleanup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,5 @@
 import React from "react"
-import DateRangePicker from "components/alpha/DateRangePicker"
 
 export default function Index() {
-  const [value, setValue] = React.useState("-7d")
-
-  return (
-    <main className="flex items-start p-6 space-x-96">
-      <DateRangePicker
-        options={[
-          {
-            label: "Today",
-            value: "today",
-          },
-          {
-            label: "Yesterday",
-            value: "yesterday",
-          },
-          {
-            label: "Last 7 days",
-            value: "-7d",
-          },
-          {
-            label: "Last 30 days",
-            value: "-30d",
-          },
-          {
-            label: "Last 12 months",
-            value: "-12M",
-          },
-        ]}
-        value={value}
-        onValueChange={setValue}
-      />
-      <pre>{JSON.stringify(value, null, 2)}</pre>
-    </main>
-  )
+  return <main className="p-6">It works!</main>
 }
