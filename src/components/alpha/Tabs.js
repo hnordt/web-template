417f7cc10f819785b815dd188f8f605b8bbfd7d7
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-03-25 09:24:59 -0300

    feat: Add Tabs

---
+++ src/components/alpha/Tabs.js
@@ -0,0 +1,102 @@
+import React from "react"
+import {
+  TabList as BaseTabList,
+  Tab as BaseTab,
+  TabPanel as BaseTabPanel,
+  useTabState,
+} from "reakit"
+import cn from "classnames"
+
+const TabsContext = React.createContext()
+
+export default function Tabs(props) {
+  const tabState = useTabState({
+    orientation: props.orientation,
+  })
+
+  return (
+    <TabsContext.Provider
+      value={{
+        tabState,
+      }}
+    >
+      {props.children}
+    </TabsContext.Provider>
+  )
+}
+
+export function TabList(props) {
+  const tabs = React.useContext(TabsContext)
+
+  return (
+    <BaseTabList
+      {...tabs.tabState}
+      className={
+        tabs.tabState.orientation === "vertical"
+          ? "space-y-1"
+          : "flex space-x-4"
+      }
+      aria-label={props["aria-label"]}
+    >
+      {props.children}
+    </BaseTabList>
+  )
+}
+
+export function Tab(props) {
+  const tabs = React.useContext(TabsContext)
+
+  return (
+    <BaseTab
+      {...tabs.tabState}
+      className={
+        tabs.tabState.orientation === "vertical"
+          ? cn(
+              "group flex items-center px-3 py-2 w-full text-sm font-medium rounded-md focus:outline-none focus-visible:ring",
+              props.id === tabs.tabState.selectedId
+                ? "text-gray-900 bg-gray-100"
+                : "text-gray-600 hover:text-gray-900 hover:bg-gray-50"
+            )
+          : cn(
+              "inline-flex items-center px-3 py-2 text-sm font-medium rounded-md focus:outline-none focus-visible:ring",
+              props.id === tabs.tabState.selectedId
+                ? "text-gray-700 bg-gray-100"
+                : "group text-gray-500 hover:text-gray-700"
+            )
+      }
+      id={props.id}
+      disabled={props.disabled}
+    >
+      {props.icon &&
+        React.createElement(props.icon, {
+          className:
+            tabs.tabState.orientation === "vertical"
+              ? cn(
+                  "flex-shrink-0 -ml-1 mr-3 w-6 h-6",
+                  props.id === tabs.tabState.selectedId
+                    ? "text-gray-500"
+                    : "text-gray-400 group-hover:text-gray-500"
+                )
+              : cn(
+                  "flex-shrink-0 mr-2 w-5 h-5",
+                  props.id === tabs.tabState.selectedId
+                    ? "text-gray-500"
+                    : "text-gray-400 group-hover:text-gray-500"
+                ),
+        })}
+      <span
+        className={
+          tabs.tabState.orientation === "vertical" ? "truncate" : undefined
+        }
+      >
+        {props.children}
+      </span>
+    </BaseTab>
+  )
+}
+
+export function TabPanel(props) {
+  const tabs = React.useContext(TabsContext)
+
+  return <BaseTabPanel {...tabs.tabState}>{props.children}</BaseTabPanel>
+}
