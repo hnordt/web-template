47db4a5824ee668556bfbe7a7e9d62451e0fffa3
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-07-21 11:59:26 -0300

    feat: Add Layout

---
+++ src/components/Layout.tsx
@@ -0,0 +1,52 @@
+import React from "react"
+import { useHistory } from "react-router-dom"
+
+export default function Layout(props) {
+  const history = useHistory()
+
+  return (
+    <main className="p-6">
+      <div className="mx-auto w-full max-w-6xl">
+        <div className="mb-8">
+          <div className="flex items-center justify-between">
+            <div>
+              <h1 className="text-gray-900 text-2xl font-bold">
+                {props.title}
+              </h1>
+              {props.description && (
+                <p className="text-gray-500 text-sm">{props.description}</p>
+              )}
+            </div>
+            {props.actions?.map((action) => (
+              <button
+                key={action.label}
+                className="inline-flex items-center px-4 py-2 text-white text-sm font-medium bg-green-600 hover:bg-green-700 border border-transparent rounded-md focus:outline-none shadow-sm focus:ring-2 focus:ring-green-500 focus:ring-offset-2"
+                type="button"
+                onClick={() => {
+                  if (typeof action.onClick === "object") {
+                    if (action.onClick.push) {
+                      history.push(action.onClick.push)
+                    }
+
+                    return
+                  }
+
+                  action.onClick?.()
+                }}
+              >
+                {action.label}
+              </button>
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block align-middle py-2 min-w-full sm:px-6 lg:px-8">
+              {props.children}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  )
+}
