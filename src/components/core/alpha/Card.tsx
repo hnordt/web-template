2fed92ce28abb6dc7ab8e15669822ff5921559f2
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-07-23 11:08:09 -0300

    chore: Upgrade packages

--- src/components/core/alpha/Card.tsx
+++ src/components/core/alpha/Card.tsx
@@ -2,6 +2,8 @@ import React from "react"
 
 export default function Card(props) {
   return (
-    <div className="shadow overflow-hidden sm:rounded-lg">{props.children}</div>
+    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+      {props.children}
+    </div>
   )
 }
