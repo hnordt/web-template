006534b535a338ee049a7321b5c434a01556ef7f
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-07-26 19:41:51 -0300

    chore: Add useHandleEvent

---
+++ src/hooks/useHandleEvent.ts
@@ -0,0 +1,95 @@
+import { useHistory } from "react-router-dom"
+import {
+  UseQueryResult,
+  UseMutationResult,
+  UseMutationOptions,
+} from "react-query"
+import toast from "react-hot-toast"
+
+export type HandleEventConfig =
+  | {
+      /** Triggers the mutation */
+      mutate?:
+        | UseMutationResult
+        | [
+            mutation: UseMutationResult,
+            variables: any,
+            options?: UseMutationOptions
+          ]
+      /** Triggers the mutation */
+      mutateAsync?: [
+        mutation: UseMutationResult,
+        variables: any,
+        options?: UseMutationOptions,
+        handlers?: {
+          then?: HandleEventConfig
+          catch?: HandleEventConfig
+        }
+      ]
+      /** Shows a toast */
+      toast?: ["success" | "error", string]
+      /** Refetches the query */
+      refetch?: UseQueryResult
+      /** Pushes new entry to browser history */
+      push?: string | { search?: string }
+    }
+  | ((...args: any) => HandleEventConfig | void)
+
+export default function useHandleEvent() {
+  const history = useHistory()
+
+  function handleEvent(config: HandleEventConfig) {
+    return (...args) => {
+      if (typeof config === "function") {
+        const result = config(...args)
+
+        if (typeof result === "object") {
+          // TODO: do we need to return?
+          return handleEvent(result)(...args)
+        }
+
+        // TODO: do we need to return?
+        return result
+      }
+
+      if (config.mutate) {
+        if (Array.isArray(config.mutate)) {
+          config.mutate[0].mutate(config.mutate[1], config.mutate[2])
+        } else {
+          config.mutate.mutate(undefined)
+        }
+      }
+
+      if (config.mutateAsync) {
+        config.mutateAsync[0]
+          .mutateAsync(config.mutateAsync[1], config.mutateAsync[2])
+          .then((data) => {
+            if (config.mutateAsync[3]?.then) {
+              handleEvent(config.mutateAsync[3].then)(data)
+            }
+          })
+          .catch((error) => {
+            if (config.mutateAsync[3]?.catch) {
+              handleEvent(config.mutateAsync[3].catch)(error)
+            } else {
+              throw error
+            }
+          })
+      }
+
+      if (config.toast) {
+        toast[config.toast[0]](config.toast[1])
+      }
+
+      if (config.refetch) {
+        config.refetch.refetch()
+      }
+
+      if (config.push) {
+        history.push(config.push)
+      }
+    }
+  }
+
+  return handleEvent
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,71 @@
 import React from "react"
+import useHandleEvent from "hooks/useHandleEvent"
+import { useMutation } from "react-query"
+import axios from "axios"
+import { TrashIcon } from "@heroicons/react/outline"
+
+function Button(props) {
+  const { loading, ...rest } = props
+
+  return (
+    <button type="button" {...rest} disabled={loading || rest.disabled}>
+      {loading ? "..." : rest.children}
+    </button>
+  )
+}
 
 export default function HomeScreen() {
-  return <main className="p-6">It works!</main>
+  const handleEvent = useHandleEvent()
+
+  const deleteUserMutation = useMutation(
+    "users",
+    (userId) =>
+      new Promise(
+        (
+          resolve,
+          reject //reject(new Error("test"))
+        ) =>
+          setTimeout(
+            () =>
+              axios
+                .put(`https://jsonplaceholder.typicode.com/users/${userId}`, {
+                  id: userId,
+                })
+                .then((res) => res.data)
+                .then(resolve),
+            500
+          )
+      ),
+    {
+      onError: () => {},
+    }
+  )
+
+  return (
+    <main className="p-6">
+      <Button
+        loading={deleteUserMutation.isLoading}
+        onClick={handleEvent((e) => ({
+          mutateAsync: [
+            deleteUserMutation,
+            2,
+            undefined,
+            {
+              then: (data) => ({
+                toast: ["success", "Foo"],
+                push: {
+                  search: `?n=${data.id}`,
+                },
+              }),
+              catch: (e) => ({
+                toast: ["error", e.message],
+              }),
+            },
+          ],
+        }))}
+      >
+        <TrashIcon className="w-5 h-5 text-gray-400" />
+      </Button>
+    </main>
+  )
 }
