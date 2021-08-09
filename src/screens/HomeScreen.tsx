8fa3066fc295e50264d2683a168f69417e1073b9
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-08-09 15:14:54 -0300

    chore: Update packages

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,51 @@
 import React from "react"
+import { Controller, useForm } from "react-hook-form"
+import axios from "axios"
+import Select from "components/core/alpha/Select"
 
 export default function HomeScreen() {
-  return <main className="p-6">It works!</main>
+  const form = useForm({
+    defaultValues: {
+      users: [2, 6],
+    },
+  })
+
+  return (
+    <main className="p-6">
+      <div className="flex flex-col items-center justify-center h-screen space-y-4">
+        <form
+          onSubmit={form.handleSubmit((values) =>
+            console.log({
+              values,
+            })
+          )}
+        >
+          {/* <input {...form.register("users")} type="text" /> */}
+          <Controller
+            control={form.control}
+            name="users"
+            render={(props) => (
+              <Select
+                {...props.field}
+                className="w-96"
+                multiple
+                loadOptions={(query) =>
+                  axios
+                    .get(
+                      `https://jsonplaceholder.typicode.com/users${
+                        query ? `?q=${query}` : ""
+                      }`
+                    )
+                    .then((res) => res.data)
+                }
+                getOptionLabel={(user) => user.name}
+                getOptionValue={(user) => user.id}
+              />
+            )}
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </main>
+  )
 }
