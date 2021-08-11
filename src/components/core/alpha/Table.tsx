3f7feff3e0bf4fecbaffe1d5248c963cc87a7009
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-08-10 21:48:40 -0300

    chore: Improve table

--- src/components/core/alpha/Table.tsx
+++ src/components/core/alpha/Table.tsx
@@ -1,20 +1,24 @@
 import React from "react"
-import { UseQueryResult } from "react-query"
+import { UseQueryResult, UseInfiniteQueryResult } from "react-query"
 import { PlusIcon, XCircleIcon } from "@heroicons/react/solid"
-import { useTable } from "react-table"
+import { useTable, useBlockLayout } from "react-table"
+import { Virtuoso } from "react-virtuoso"
 import { useDeepCompareMemo } from "use-deep-compare"
 import cn from "classnames"
+import Loader from "components/core/alpha/Loader"
 
-interface TableProps {
+export interface TableProps {
   columns: Array<{
     variant?: "primary" | "secondary" | "tertiary"
     label: string
     accessor: string | ((row: any, rowIndex: number) => any)
+    width?: number
     align?: "center" | "right"
     renderContent?: (props: { children: React.ReactNode }) => React.ReactNode
   }>
   data?: Array<any>
   query?: UseQueryResult
+  infiniteQuery?: UseInfiniteQueryResult
   emptyState: {
     icon?: React.FunctionComponent<{ className: string }>
     title?: string
@@ -31,7 +35,25 @@ interface TableProps {
     hidden?: (row) => boolean
     onClick: (row) => void
   }>
+  height?: number
+  onEndReached?: () => void
 }
+
+const VirtuosoList = React.forwardRef<any, any>((props, ref) => (
+  <div {...props} ref={ref} className="divide-gray-200 divide-y" />
+))
+
+const VirtuosoEmptyPlaceholder = React.forwardRef<any, any>((props, ref) => (
+  <div {...props} ref={ref} className="flex items-center justify-center h-full">
+    <Loader variant="dark" size="sm" />
+  </div>
+))
+
+const VirtuosoFooter = React.forwardRef<any, any>((props, ref) => (
+  <div {...props} ref={ref} className="px-6 py-5 border-t border-gray-200">
+    <Loader variant="dark" size="sm" />
+  </div>
+))
 
 export default function Table(props: TableProps) {
   // TODO: this memoization will probably not work because
@@ -40,58 +62,60 @@ export default function Table(props: TableProps) {
     () =>
       props.columns.map((column) => ({
         Header: column.label,
-        Cell: (_props) => {
-          if (props.query?.status === "loading") {
-            return (
-              <span
-                className={cn(
-                  "block h-4 bg-gray-200 rounded animate-pulse",
-                  ["w-10/12", "w-5/12", "w-9/12"][_props.row.index]
-                )}
-              />
-            )
-          }
-
-          return (
-            <span
-              className={
-                {
-                  primary: "font-medium",
-                  tertiary: "text-gray-500",
-                }[column.variant] ?? ""
-              }
-            >
-              {column.renderContent?.({
-                children: _props.value,
-              }) ?? (
-                // Returning a React Fragment will avoid crashes when props.value
-                // is null or undefined.
-                // We don't want to cast it to string otherwise "undefined" or
-                // "null" would be rendered.
-                <>{_props.value}</>
-              )}
-            </span>
-          )
-        },
+        Cell: (_props) => (
+          <div
+            className={cn(
+              "truncate",
+              {
+                primary: "font-medium",
+                tertiary: "text-gray-500",
+              }[column.variant]
+            )}
+          >
+            {column.renderContent?.({
+              children: _props.value,
+            }) ?? (
+              // Returning a React Fragment will avoid crashes when props.value
+              // is null or undefined.
+              // We don't want to cast it to string otherwise "undefined" or
+              // "null" would be rendered.
+              <>{_props.value}</>
+            )}
+          </div>
+        ),
         accessor: column.accessor,
+        width: column.width ?? 200,
       })),
-    [props.columns, props.query?.status]
+    [props.columns, props.query?.status, props.infiniteQuery?.status]
   )
 
   const data = useDeepCompareMemo(() => props.data, [props.data])
 
-  const table = useTable({
-    columns,
-    data: props.query
-      ? props.query.status === "loading"
-        ? new Array(3).fill({})
-        : props.query.status === "error"
-        ? []
-        : props.query.data
-      : data,
-  })
+  const table = useTable(
+    {
+      columns,
+      data: props.query
+        ? props.query.status === "loading"
+          ? new Array(props.columns.length).fill({})
+          : props.query.status === "error"
+          ? []
+          : props.query.data
+        : props.infiniteQuery
+        ? props.infiniteQuery.status === "loading"
+          ? []
+          : props.infiniteQuery.status === "error"
+          ? []
+          : props.infiniteQuery.data
+        : data,
+    },
+    useBlockLayout
+  )
 
-  if (props.query?.status === "success" && !(props.query?.data as []).length) {
+  if (
+    (props.query?.status === "success" && !(props.query?.data as []).length) ||
+    (props.infiniteQuery?.status === "success" &&
+      !(props.infiniteQuery?.data as []).length)
+  ) {
     return (
       <div className={cn("text-center", props.emptyState.padding && "p-8")}>
         {props.emptyState.icon &&
@@ -145,80 +169,103 @@ export default function Table(props: TableProps) {
   }
 
   return (
-    <table
+    <div
       className="min-w-full divide-gray-200 divide-y"
       {...table.getTableProps()}
     >
-      <thead className="bg-gray-50">
+      <div className="bg-gray-50">
         {table.headerGroups.map((headerGroup) => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
+          <div {...headerGroup.getHeaderGroupProps()}>
             {headerGroup.headers.map((column) => (
-              <th
+              <div
                 className="px-6 py-3 text-left text-gray-500 text-xs font-medium tracking-wider uppercase"
                 {...column.getHeaderProps()}
               >
                 {column.render("Header")}
-              </th>
+              </div>
             ))}
             {props.actions && props.query?.status !== "loading" && (
-              <th className="px-6 py-3" />
+              <div className="px-6 py-3" />
             )}
-          </tr>
+          </div>
         ))}
-      </thead>
-      <tbody
-        className="bg-white divide-gray-200 divide-y"
-        {...table.getTableBodyProps()}
-      >
-        {table.rows.map((row) => {
-          table.prepareRow(row)
-
-          return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map((cell) => (
-                <td
-                  className="px-6 py-4 text-gray-900 whitespace-nowrap text-sm"
-                  {...cell.getCellProps()}
-                >
-                  {cell.render("Cell")}
-                </td>
-              ))}
-              {props.actions &&
-                props.actions &&
-                props.query?.status !== "loading" && (
-                  <td className="px-6 py-4 text-right whitespace-nowrap text-sm font-medium">
-                    <span className="flex flex-shrink-0 items-center justify-end space-x-4">
-                      {props.actions
-                        .filter((action) => !action.hidden?.(row.original))
-                        .map((action, i) => (
-                          <React.Fragment
-                            key={action.label ?? action.icon?.name}
-                          >
-                            {i > 0 && (
-                              <span className="text-gray-300" aria-hidden>
-                                |
-                              </span>
-                            )}
-                            <button
-                              className="text-blue-600 hover:text-blue-900 focus-visible:underline"
-                              type="button"
-                              onClick={() => action.onClick?.(row.original)}
+      </div>
+      <div className="bg-white" {...table.getTableBodyProps()}>
+        <Virtuoso
+          itemContent={(i) => {
+            const row = table.rows[i]
+            table.prepareRow(row)
+            return (
+              <div
+                {...row.getRowProps({
+                  style: {},
+                })}
+              >
+                {row.cells.map((cell) => (
+                  <div
+                    className="px-6 py-4 text-gray-900 whitespace-nowrap text-sm overflow-hidden"
+                    {...cell.getCellProps()}
+                  >
+                    {cell.render("Cell")}
+                  </div>
+                ))}
+                {props.actions &&
+                  props.actions &&
+                  props.query?.status !== "loading" && (
+                    <div className="px-6 py-4 text-right whitespace-nowrap text-sm font-medium">
+                      <span className="flex flex-shrink-0 items-center justify-end space-x-4">
+                        {props.actions
+                          .filter((action) => !action.hidden?.(row.original))
+                          .map((action, i) => (
+                            <React.Fragment
+                              key={action.label ?? action.icon?.name}
                             >
-                              {action.icon
-                                ? React.createElement(action.icon, {
-                                    className: "w-5 h-5 text-gray-400",
-                                  })
-                                : action.label}
-                            </button>
-                          </React.Fragment>
-                        ))}
-                    </span>
-                  </td>
-                )}
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+                              {i > 0 && (
+                                <span className="text-gray-300" aria-hidden>
+                                  |
+                                </span>
+                              )}
+                              <button
+                                className="text-blue-600 hover:text-blue-900 focus-visible:underline"
+                                type="button"
+                                onClick={() => action.onClick?.(row.original)}
+                              >
+                                {action.icon
+                                  ? React.createElement(action.icon, {
+                                      className: "w-5 h-5 text-gray-400",
+                                    })
+                                  : action.label}
+                              </button>
+                            </React.Fragment>
+                          ))}
+                      </span>
+                    </div>
+                  )}
+              </div>
+            )
+          }}
+          totalCount={table.rows.length}
+          endReached={() => {
+            if (
+              props.infiniteQuery?.hasNextPage &&
+              !props.infiniteQuery?.isFetchingNextPage
+            ) {
+              props.infiniteQuery.fetchNextPage()
+            }
+            props.onEndReached?.()
+          }}
+          components={{
+            List: VirtuosoList,
+            EmptyPlaceholder: VirtuosoEmptyPlaceholder,
+            Footer: props.infiniteQuery?.isFetchingNextPage
+              ? VirtuosoFooter
+              : undefined,
+          }}
+          style={{
+            height: props.height ?? 423 /* 423px = 8 items */,
+          }}
+        />
+      </div>
+    </div>
   )
 }
