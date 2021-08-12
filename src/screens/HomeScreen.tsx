731577a9ec80e1705a2ef60375cf81cda87e4456
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-08-12 10:54:01 -0300

    chore: Update next.env.ts

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,15 @@
 import React from "react"
+import { Virtuoso } from "react-virtuoso"
 
 export default function HomeScreen() {
-  return <div className="p-6">It works!</div>
+  return (
+    <div className="p-6">
+      <div className="h-[400px]">
+        <Virtuoso
+          itemContent={(index) => <div>Item {index}</div>}
+          totalCount={200}
+        />
+      </div>
+    </div>
+  )
 }
