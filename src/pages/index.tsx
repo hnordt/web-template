1eb41fdc2dcdac1ed4c42d7a7188dd57481faa52
Heliton Nordt <1625399+hnordt@users.noreply.github.com>
2021-10-18 19:30:47 -0300

    chore: Auto reports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import React from "react"
-import Form, { FormInput } from "components/core/Form"
-import ModalForm from "components/core/ModalForm"
+import dayjs from "dayjs"
+import Form, { FormField, FormActions, FormButton } from "components/core/Form"
+import Input from "components/core/Input"
+import RadioGroup from "components/core/RadioGroup"
+import Select from "components/core/Select"
+import Modal from "components/core/Modal"
 
 const reports = [
   {
@@ -58,199 +62,229 @@ const recipients = [
 export default function HomeScreen() {
   return (
     <div className="p-6">
-      <ModalForm
+      <Modal
         title="Create report"
         description="Fill in the information below to create an automated report"
-        fields={[
-          {
-            type: "text",
-            label: "Report name",
-            name: "name",
-            help: "Enter the report name for self reference",
-            size: 6,
-            required: "Required",
-          },
-          {
-            type: "date",
-            label: "Start date",
-            name: "startDate",
-            help: "Date where the report will start sending",
-            size: 6,
-            required: "Required",
-          },
-          {
-            type: "select",
-            label: "Recipients",
-            name: "recipients",
-            options: recipients.map((r) => ({
-              label: r.name,
-              value: r.id,
-            })),
-            placeholder: "All",
-            help: "The owner of this report will automatically receive it",
-            size: 12,
-            multiple: true,
-            messages: {
-              noOptions: () => "No recipients found",
-            },
-          },
-          {
-            type: "select",
-            label: "Controllers",
-            name: "controllers",
-            options: controllers.map((c) => ({
-              label: c.name,
-              value: c.id,
-            })),
-            placeholder: "All",
-            help: "Select the controllers you wish to add to your report",
-            size: 12,
-            required: "Required",
-            multiple: true,
-            messages: {
-              noOptions: () => "No controllers found",
-            },
-          },
-          {
-            type: "radio",
-            label: "Settings",
-            name: "settings",
-            options: [
-              {
-                label: "Same settings",
-                description:
-                  "All controllers have the same delivery frequency and reports",
-                value: "same",
-              },
-              {
-                label: "Different settings",
-                description:
-                  "Controllers have different delivery frequency and reports",
-                value: "different",
-              },
-            ],
-            size: 12,
-          },
-          {
-            type: "select",
-            label: "Frequency",
-            name: "frequency",
-            options: [
-              {
-                label: "Daily",
-                value: "daily",
-              },
-              {
-                label: "Weekly",
-                value: "weekly",
-              },
-              {
-                label: "Monthly",
-                value: "monthly",
-              },
-            ],
-            size: 3,
-            required: "Required",
-            hidden: (values) => values.settings === "different",
-          },
-          {
-            type: "select",
-            label: "Reports",
-            name: "reports",
-            options: reports.map((report) => ({
-              label: report.name,
-              value: report.id,
-            })),
-            placeholder: "All",
-            size: 9,
-            hidden: (values) => values.settings === "different",
-            multiple: true,
-          },
-        ]}
-        defaultValues={{
-          name: "",
-          startDate: "",
-          recipients: [],
-          controllers: controllers.map((controller) => controller.id),
-          settings: "different",
-          frequency: "daily",
-          reports: [],
-        }}
-        renderContent={(props) => (
-          <>
-            {props.children}
-            {props.values.settings === "different" &&
-              props.values.controllers.length > 0 && (
-                <ul className="mt-6 border rounded-md divide-y">
-                  {props.values.controllers.map((controllerId) => {
-                    const controller = controllers.find(
-                      (controller) => controller.id === controllerId
-                    )
-                    return (
-                      <li key={controller.id}>
-                        <div className="flex items-baseline justify-between px-4 py-1 bg-gray-100 border-b space-x-4">
-                          <div className="flex items-baseline space-x-2">
-                            <h1 className="text-sm font-medium">
-                              {controller.site} - {controller.name} -{" "}
-                              <span className="font-normal">
-                                {controller.id}
-                              </span>
-                            </h1>
-                          </div>
-                          <p className="text-gray-500 text-xs">
-                            {controller.version}
-                          </p>
-                        </div>
-                        <div className="flex p-4 space-x-4">
-                          {props.registerField(
-                            <FormInput
-                              className="w-36"
-                              type="select"
-                              name={`frequenciesByController[${controller.id}]`}
-                              options={[
-                                {
-                                  label: "Daily",
-                                  value: "daily",
-                                },
-                                {
-                                  label: "Weekly",
-                                  value: "weekly",
-                                },
-                                {
-                                  label: "Monthly",
-                                  value: "monthly",
-                                },
-                              ]}
-                              defaultValue="daily"
-                            />
-                          )}
-                          {props.registerField(
-                            <FormInput
-                              className="flex-1"
-                              type="select"
-                              name={`reportsByController[${controller.id}]`}
-                              options={reports.map((report) => ({
-                                label: report.name,
-                                value: report.id,
-                              }))}
-                              placeholder="All reports"
-                              defaultValue={[]}
-                              multiple
-                            />
-                          )}
-                        </div>
-                      </li>
-                    )
-                  })}
-                </ul>
-              )}
-          </>
-        )}
         size="3xl"
         open
-        onSubmit={(values) => console.info(values)}
         onClose={() => {}}
-      />
+      >
+        <Form
+          defaultValues={{
+            name: "",
+            startDate: dayjs().format("YYYY-MM-DD"),
+            recipients: [],
+            controllers: [],
+            settings: "same",
+            frequency: "daily",
+            reports: [],
+          }}
+          watch={["controllers", "settings"]}
+          onSubmit={(values) => console.log("handleSubmit", values)}
+        >
+          {({ values }) => (
+            <>
+              <FormField
+                name="name"
+                label="Report name"
+                hint="Enter the report name for self reference"
+                span={6}
+                required
+              >
+                <Input type="text" />
+              </FormField>
+              <FormField
+                name="startDate"
+                label="Start date"
+                hint="Date where the report will start sending"
+                span={6}
+                required
+              >
+                <Input type="date" />
+              </FormField>
+              <FormField
+                name="recipients"
+                label="Recipients"
+                hint="The owner of this report will automatically receive it"
+                span={12}
+              >
+                <Select
+                  options={recipients.map((r) => ({
+                    label: r.name,
+                    value: r.id,
+                  }))}
+                  placeholder="All"
+                  multiple
+                  messages={{
+                    noOptions: () => "No recipients found",
+                  }}
+                />
+              </FormField>
+              <FormField
+                name="controllers"
+                label="Controllers"
+                hint="Select the controllers you wish to add to your report"
+                span={12}
+                required={
+                  values.settings === "different"
+                    ? "When enabling different settings per controller you should select at least one controller"
+                    : false
+                }
+              >
+                <Select
+                  options={controllers.map((r) => ({
+                    label: r.name,
+                    value: r.id,
+                  }))}
+                  placeholder="All"
+                  multiple
+                  messages={{
+                    noOptions: () => "No controllers found",
+                  }}
+                />
+              </FormField>
+              <FormField name="settings" label="Controller settings" span={12}>
+                <RadioGroup
+                  options={[
+                    {
+                      label: "Similar",
+                      description:
+                        "All controllers have the same delivery frequency and reports",
+                      value: "same",
+                    },
+                    {
+                      label: "Different",
+                      description:
+                        "Controllers have different delivery frequency and reports",
+                      value: "different",
+                    },
+                  ]}
+                />
+              </FormField>
+              {values.settings === "same" ? (
+                <>
+                  <FormField
+                    name="frequency"
+                    label="Frequency"
+                    span={3}
+                    required
+                  >
+                    <Select
+                      options={[
+                        {
+                          label: "Daily",
+                          value: "daily",
+                        },
+                        {
+                          label: "Weekly",
+                          value: "weekly",
+                        },
+                        {
+                          label: "Monthly",
+                          value: "monthly",
+                        },
+                      ]}
+                      searchable={false}
+                      clearable={false}
+                    />
+                  </FormField>
+                  <FormField name="reports" label="Reports" span={9}>
+                    <Select
+                      options={reports.map((report) => ({
+                        label: report.name,
+                        value: report.id,
+                      }))}
+                      placeholder="All"
+                      multiple
+                      searchable={false}
+                    />
+                  </FormField>
+                </>
+              ) : (
+                values.controllers.length > 0 && (
+                  <div className="col-span-12 mt-2">
+                    <ul className="border rounded-md divide-y">
+                      {values.controllers.map((controllerId) => {
+                        const controller = controllers.find(
+                          (controller) => controller.id === controllerId
+                        )
+
+                        return (
+                          <li key={controller.id}>
+                            <div className="flex items-baseline justify-between px-4 py-1 bg-gray-100 border-b space-x-4">
+                              <div className="flex items-baseline space-x-2">
+                                <h1 className="text-sm font-medium">
+                                  {controller.site} - {controller.name} -{" "}
+                                  <span className="font-normal">
+                                    {controller.id}
+                                  </span>
+                                </h1>
+                              </div>
+                              <p className="text-gray-500 text-xs">
+                                {controller.version}
+                              </p>
+                            </div>
+                            <div className="flex p-4 bg-white space-x-4">
+                              <FormField
+                                className="w-36"
+                                name={`frequenciesByController[${controller.id}]`}
+                                defaultValue="daily"
+                                required
+                              >
+                                <Select
+                                  options={[
+                                    {
+                                      label: "Daily",
+                                      value: "daily",
+                                    },
+                                    {
+                                      label: "Weekly",
+                                      value: "weekly",
+                                    },
+                                    {
+                                      label: "Monthly",
+                                      value: "monthly",
+                                    },
+                                  ]}
+                                  searchable={false}
+                                  clearable={false}
+                                />
+                              </FormField>
+                              <FormField
+                                className="flex-1"
+                                name={`reportsByController[${controller.id}]`}
+                                defaultValue={[]}
+                              >
+                                <Select
+                                  options={reports.map((report) => ({
+                                    label: report.name,
+                                    value: report.id,
+                                  }))}
+                                  placeholder="All reports"
+                                  multiple
+                                  searchable={false}
+                                />
+                              </FormField>
+                            </div>
+                          </li>
+                        )
+                      })}
+                    </ul>
+                  </div>
+                )
+              )}
+              <FormActions>
+                <FormButton type="reset" variant="secondary">
+                  Cancel
+                </FormButton>
+                <FormButton type="submit" variant="primary">
+                  Submit
+                </FormButton>
+              </FormActions>
+            </>
+          )}
+        </Form>
+      </Modal>
     </div>
   )
 }
